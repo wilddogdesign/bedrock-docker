954a753dac765d35a101b86ac036840482a3bba9
Tristan Brookes <tristanbrookes@wilddogdesign.co.uk>
2017-07-11 17:43:52 +0100

    Deployment changes to use nvm

--- deploy/config/deploy/staging.rb
+++ deploy/config/deploy/staging.rb
@@ -15,20 +15,17 @@ set :vhosts_path, '/Path/to/wherever'
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-# set :ssh_options, {
-#   keys: %w(~/.ssh/id_rsa),
-#   forward_agent: false,
-#   auth_methods: %w(password)
-# }
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+  # forward_agent: false,
+  # auth_methods: %w(password)
+}
 
 set :branch, :staging
 
 set :default_env, { path: "/path/to/bin:$PATH" }
 
-SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
-SSHKit.config.command_map[:node] = "/usr/local/bin/node"
-SSHKit.config.command_map[:npm] = "/usr/local/bin/npm"
-SSHKit.config.command_map[:gulp] = "/usr/local/bin/gulp"
-SSHKit.config.command_map[:jspm] = "/usr/local/bin/jspm"
+set :nvm_node, 'v7.5.0'
+set :nvm_map_bins, %w{node npm jspm bower}
 
 fetch(:default_env).merge!(wp_env: :staging)
